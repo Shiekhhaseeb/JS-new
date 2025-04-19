dfd50431490b566fe20970bab31812a869f1378e
Shiekh Haseeb <shiekhhaseeb2019@gmail.com>
2025-04-20 00:00:51 +0530

    changes in variables file

---
+++ 01-variables.js
@@ -0,0 +1,22 @@
+//variables
+const accountId=144553;
+
+let accountEmail="haseeb@gmail.com";
+
+var accountPassword="12345";
+
+accountCity ="Jaipur";
+
+let accountState;//undefined dega
+ 
+// accountId=2;//not allowed
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="Bengaluru"
+
+console.log(accountId);
+/*prefer not to use var becoz of issue in block scope and  fxnl scope*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+ 
