1cc5d4a508611efe0ab62d9b1984b5a8c8b4c8ca
Shiekh Haseeb <shiekhhaseeb2019@gmail.com>
2025-04-20 16:18:00 +0530

    New file truthy

--- truthy.js
+++ truthy.js
@@ -1,13 +1,10 @@
-const userEmail =[]
+const userEmail = [];
 
 if (userEmail) {
-    console.log("Got user email");
-    
+  console.log("Got user email");
+} else {
+  console.log("Don't have user email");
 }
-else{
-    console.log("Don't have user email");
-    
-} 
 //falsy values
 // 1.false, 0,-0, BigInt 0n ,"", null, undefined ,NaN
 
@@ -16,12 +13,35 @@ else{
 
 //To check array is empty
 // if (userEmail.length===0) {
-    // console.log("Array is empty");
+// console.log("Array is empty");
 // }
 
 // To check object is empty
-const emptyObj={}
-if (Object.keys(emptyObj).length===0) {
-    console.log("Object is empty");
-
+const emptyObj = {};
+if (Object.keys(emptyObj).length === 0) {
+  console.log("Object is empty");
 }
+//Nullish coalescing operator (??) :null,undefined
+
+// let val1=5 ?? 10;
+/*********************** */
+// console.log(val1);
+// let val1;
+//  val1=null ?? 10;
+
+// console.log(val1);
+/************************ */
+// let val1;
+// val1=undefined ?? 15;
+// console.log(val1);
+/************* */
+let val1;
+val1 = null ?? 10 ?? 20;
+console.log(val1);
+
+/*************************** */
+//Ternary operator
+// condition ? true ? false;
+const iceTeaPrice = 100;
+iceTeaPrice <=80 ? console.log("Less than 80") : console.log("More than 80");
+;
