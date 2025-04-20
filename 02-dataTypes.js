086486c75644de4f90ffc9d501124c1eab6eab17
Shiekh Haseeb <shiekhhaseeb2019@gmail.com>
2025-04-20 10:59:41 +0530

    2-dataTypes and Emac

---
+++ 02-dataTypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 +3); can be used in node with different syntax,this is used in browser
+//In react we try to avoid (;)
+
+console.log(3 + 3);
+// code readability should be high
+console.log("Haseeb");
+
