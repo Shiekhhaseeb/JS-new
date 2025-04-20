4ba32b18ed1259a6e8bc7461cf67c0447727cd4f
Shiekh Haseeb <shiekhhaseeb2019@gmail.com>
2025-04-20 19:13:38 +0530

    New file truthy

---
+++ 29.doWhile.js
@@ -0,0 +1,32 @@
+// while (condition) {
+
+// }
+// let index=0;
+// while (index<=10) {
+//    console.log(`value of index is ${index}`);
+
+// index++;
+// }
+/******************************* */
+// let myArray = ["Flash", "batman", "superman"];
+
+// let arr = 0;//start from 0
+// while (arr < myArray.length) {//myArray.length is 3 (because there are 3 items).
+
+//   console.log(`value is ${myArray[arr]}`);
+//   arr = arr + 1;
+// }
+/*********************************** */
+// let score = 1;
+
+// do {
+//   console.log(`Score is ${score}`);
+//   score++;
+// } while (score <= 10);
+/************** */
+let score = 11;
+
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
