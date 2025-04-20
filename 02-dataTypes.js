e74d9caded54110d03a92b1983b0beb6198b84ad
Shiekh Haseeb <shiekhhaseeb2019@gmail.com>
2025-04-20 11:15:12 +0530

    DataTypes learned

--- 02-dataTypes.js
+++ 02-dataTypes.js
@@ -6,4 +6,21 @@
 console.log(3 + 3);
 // code readability should be high
 console.log("Haseeb");
+//DATAtypes in JS
+let name = "Haseeb"//string
+let age =18;  //number
+let isLoggedIn=true;//boolean => true or false
+
+//1.number=> 2 to the power 53
+//2.bifint
+//3.string
+//4.Boolean
+//5.null
+//6.undefined
+//7.Symbol:-used to check uniquesness used in react
+console.log(typeof "Haseeb")
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
 
