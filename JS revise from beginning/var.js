267e4b051ce12710cdc441c32598653b5b3643c7
Shiekh Haseeb <shiekhhaseeb2019@gmail.com>
2025-06-03 13:09:10 +0530

    let const and var

---
+++ JS revise from beginning/var.js
@@ -0,0 +1,41 @@
+// var age =25;
+// console.log(age);
+/*************** */
+
+
+/************* */
+//difference b/w let,const and var
+//1.var:-->hoga,hoga
+
+//a.Redeclaration
+// var a=10;
+// var a=20;
+// console.log(a);
+
+// //b.Reassignment
+
+// var a=10;
+//    a=20;
+// console.log(a);
+
+//2.let
+
+//a.Redeclaration(nahi,hoga)
+// let a=10;
+// let a=20;
+// console.log(a);
+//b.Reassignment
+// let a=10;
+//    a=20;
+// console.log(a);
+
+// //3.const
+
+//a.Redeclaration
+// const a=10;
+// const a=20;
+// console.log(a);
+// //b.Reassignment
+// const a=10;
+//       a=20;
+// console.log(a);
